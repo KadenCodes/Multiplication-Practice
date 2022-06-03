a0f10ef8b04a2a534e8827d27f0401a94fc03de7
Kaden <Kaden@LAPTOP-FJN7I66Q>
2022-06-03 18:04:31 -0400

    A simple multiplication practice program made by Kaden and refined by Dottik.

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿//multiplication practice Made by Kaden (Second ever c# project).
+using System;
+using System.Text;
+namespace multiples
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            run();
+            Console.ReadKey();
+            static void run()
+            {
+                Console.OutputEncoding = Encoding.UTF8;
+                int correct = 1;
+                int incorrect = 1;
+                Console.WriteLine("What is the max multiple you would like? ");
+                int Maximum = Convert.ToInt32(Console.ReadLine());
+                for (int i = 0; i < 100; i = i++)
+                {
+                    Random rnd = new Random();
+                    int first = rnd.Next(0, Maximum);
+                    int second = rnd.Next(0, Maximum);
+                    Console.WriteLine(first + "x" + second + "= ?");
+                    int answer = Convert.ToInt32(Console.ReadLine());
+                    if (answer == first * second)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        int Tcorrect = correct++;
+                        int Tincorrect = incorrect;
+                        Console.Clear();
+                        Console.WriteLine($"{answer} ✅");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.WriteLine("Good Job!");
+                        Console.WriteLine($"Correctly solved ✅ = {Tcorrect}");
+                        Console.WriteLine($"Incorrect solved ❌ = {Tincorrect}");
+                    }
+                    if (answer != first * second)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        int Tincorrect = incorrect++;
+                        int Tcorrect = correct;
+                        Console.Clear();
+                        Console.WriteLine($"{answer} ❌");
+                        Console.WriteLine("Not quite...");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.WriteLine($"Correctly solved ✅ = {Tcorrect}");
+                        Console.WriteLine($"Incorrect solved ❌ = {Tincorrect}");
+                    }
+                }
+            }
+        }
+    }
+}
